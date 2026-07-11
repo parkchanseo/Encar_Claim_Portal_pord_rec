f223653f721142464085a951e798c095c25b7839
찬서 <pcsmhj@gmail.com>
2026-07-11 16:02:08 +0000

    .env 추가

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
-// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rgptxelbwsawjsejofwc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJncHR4ZWxid3Nhd2pzZWpvZndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTQ4OTEsImV4cCI6MjA5NjA5MDg5MX0.6cuZGtNVQhoJak_0x-BHrNW8mHw9Eq18l0ypUt3hPcY"; // Supabase 대시보드 [Project Settings > API] 에 있습니다.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
